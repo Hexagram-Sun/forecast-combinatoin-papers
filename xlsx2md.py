43c7887038d8f89e089dc4556c09184355eb293a
leo1600 <1519167809@qq.com>
2022-11-27 17:02:19 +0800

    add some papers

---
+++ xlsx2md.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def item2md(p):
+    cite = p['cite'].replace('\n', '').replace('  ', ' ')
+    name = cite[cite.find('.')+1:].strip()
+    name = name[:name.find('.')+1]
+    doi = cite[cite.find('DOI:'):]
+    cite = cite.replace(name, f'**{name}**')\
+               .replace(doi, f'[*{doi}*]({p["url"]})')
+    return f'{cite}\n\n  **brief: {p["brief"]}**'
+
+df = pd.read_excel('./papers.xlsx')
+with open('./readme.md', 'w', encoding='utf-8') as f:
+    f.write('# forecast-combinatoin-papers\n\n')
+    for i in range(len(df)):
+        f.write('+ ')
+        f.write(item2md(df.iloc[i]))
+        f.write('\n<br>\n\n')
+    
+
